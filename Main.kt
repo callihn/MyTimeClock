3446730dd32f39e12889c691b3fc304ae7ea1a8e
callihn <37760108+callihn@users.noreply.github.com>
2026-06-07 00:55:21 -0500

    Create Main.kt for language detection

---
+++ Main.kt
@@ -0,0 +1,7 @@
+package com.example.dummy
+
+// This file exists to indicate the core programming language of the application
+fun main() {
+    val appName = "Application"
+    println("Initialized $appName successfully.")
+}
